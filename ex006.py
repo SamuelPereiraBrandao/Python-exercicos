0ded14e50f0a26dea7f72beeec8c2b8a9325403a
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2021-12-17 22:35:14 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,8 +1,6 @@
 n1 = int(input("Primeiro numero:"))
-print("O seu numero eh {} e os 3 numeros sucessores deles sao: {},{} e {}".format(n1,n1+1,n1+2,n1+3))
-print("Ja, seus 3 antecessores sao: {},{} e {}".format(n1-1,n1-2,n1-3))
-#outro exercicio
-n11 =  int (input("numero:"))
-dobro= n11+n11
-triplo=n11*3
-print("O numero eh: {}, O dobro eh: {}, O triplo eh {}".format(n11,dobro,triplo))
+print("O seu numero eh {} e os 3 numeros sucessores deles sao: {},{} e {}".format(n1, n1 + 1, n1 + 2, n1 + 3))
+print("Ja, seus 3 antecessores sao: {},{} e {}".format(n1 - 1, n1 - 2, n1 - 3))
+# outro exercicio
+n11 = int(input("numero:"))
+print("O numero eh: {}, O dobro eh: {}, O triplo eh {}, e a raiz eh: {:3f}".format(n11, n11 * 2, n11 * 3, n11 ** (1 / 2)))
