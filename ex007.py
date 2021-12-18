63d748e5321688d37918d1bc863a437bd0737c2b
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2021-12-17 21:35:11 -0300

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,5 @@
+nota1= int(input("Sua primeira nota:"))
+nota2= int(input("Sua segunda nota:"))
+nota3= int(input("sua terceira nota:"))
+media= (nota1+nota2+nota3)/3
+print("Suas notas sao: {},{},{} e por isso sua media eh: {}".format(nota1,nota2,nota3,media))
