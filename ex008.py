a1b4675d71001e47c33c3785c8038a01f48b1063
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2021-12-20 12:04:52 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,3 @@
+metros = float (input("Quantos metros?"))
+
+print("Temos {}metros \nconvertidos em centimetros temos: {:.0f}cm\nE em milimitros temos: {:.0f}mm".format(metros,metros*100,metros*1000))
---
+++ ex010.py
@@ -0,0 +1,3 @@
+dinheiro = float(input("quanto dinheiro voce tem na carteira? R$"))
+dolar = dinheiro / 5.70
+print("com {:.2f} R$ voce consegue comprar {:.2f} US | Dólares".format(dinheiro, dolar))
